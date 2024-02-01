35d330ef3e63350996157b9b84bc9b5516735d0b
anibe <anibe@ANI-DESKTOP>
2024-01-31 21:03:14 -0400

    Add project files.

---
+++ Ejercicios C#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal lado;
+
+        do
+        {
+            Console.Write("Ingrese el valor de un lado del cuadrado: ");
+            string entrada = Console.ReadLine();
+
+            if (!decimal.TryParse(entrada, out lado))
+            {
+                Console.WriteLine("\n" + "El valor ingresado no es valido. Intente nuevamente." + "\n");
+            }
+
+        } while (lado == 0);
+
+        decimal Perimetro = lado * 4;
+
+        Console.WriteLine("El perímetro del cuadrado es: " + Perimetro);
+    }
+        
+}
